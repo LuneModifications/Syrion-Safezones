39d14c4d1dfc1d033d72c91b2646432a49fe8277
Syrion <137480929+SyrionDevelopment@users.noreply.github.com>
2023-08-22 18:36:52 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,16 +1,16 @@
 --================================--
---      Breezy_Safezones 1.0      --
---      by BreezyTheDev           --
---		GNU License v3.0		  --
+--      Syrion_Safezones 1.0      --
 --================================--
 
 Config = {
-    Alert = "~g~You are in a safezone.",
+    Alert = "~g~You are in a safezone. - Syrion Safezones",
     Warning = "~r~You are in a ~g~safezone. ~r~You can not shoot.",
     Radius = 50,
     Locations = {
         -- Set the locations you wish to be a safezone.
-        -- Format: {XCoord, YCoord, ZCoord}
+        -- Format: {X, Y, Z}
         {1850.04, 3687.3, 32.47}, -- Sandy PD
+        {0, 0, 0}, -- Extra
+        {0, 0, 0, -- Extra
     }
 }
